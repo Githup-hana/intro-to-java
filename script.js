17bfcd217248e63a761a1adc96c2736b24327197
Githup-hana <hanaabrham89@gmail.com>
2024-10-11 13:22:08 +0200

    feat:adding submit event form

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
-const userName =document.getElementById("name").value;
-const country=document.getElementById("country").value;
-const yearOfBrith=document.getElementById("year").value;
+const userName =document.getElementById("name");
+const country=document.getElementById("country");
+const yearOfBrith=document.getElementById("year");
 const form=document.getElementById("user-form");
-const messegeContent=document.getElementById("message-content").textContent;
+const messegeContent=document.getElementById("message-content");
+const currentYear = new Date().getFullYear();
+console.Logs(`Das Jahr ist `);
 
-const consoleLogs= () => {console.log("hallo java")
-    console.log(messegeContent);};
-    consoleLogs();
+// const consoleLogs= () => {console.log("hallo java");
+
+//     console.log(`Das Jahr ist ${currentYear}.Hurraaa!`);
+//     consoleLogs();
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    consoleLogs();
+    console.log(`Name: ${userName.value}`);
+    console.log(`country: ${country.value}`);
+    console.log(`country: ${country.value}`);
 });
